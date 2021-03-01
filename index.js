bc863134d31d944e7eb9942b9ea44030812dec14
maquino96 <matthewaquino96@gmail.com>
2021-03-01 10:13:41 -0500

    solution

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
-  const jokeList = document.getElementById('joke-list')
-  const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
-  let joke;
+  
 
   function fetchJoke(){
-    fetch('https://icanhazdadjoke.com/', {
+    return fetch('https://icanhazdadjoke.com/', {
       headers: {
-        "Accept": "application/json"
+        // 'Content-Type': 'application/json'
+        'Accept': 'application/json'
       }
     })
     .then(res => res.json())
@@ -16,12 +14,21 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const jokeList = document.getElementById('joke-list')
+    const newJokeLi = document.createElement('li')
+    const username = document.getElementById('name-input').value
+
 
     if(username === "") return;
-    fetchJoke()
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says:</span> ${joke}
-    `
-    jokeList.appendChild(newJokeLi)
+
+    fetchJoke().then(joke => { 
+      newJokeLi.innerHTML = `
+      <span class="username">${username} says:</span> ${joke}
+      `
+      jokeList.appendChild(newJokeLi)
+
+    })
+   
   })
 })
